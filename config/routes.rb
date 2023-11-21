23c36b26890b0fe9908e405c2d8bccf5afccc79f
Antoine Aube <aaube3@gmail.com>
2023-11-20 17:16:33 -0800

    routes: for all three endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/subscribe", to: "subscriptions#create"
+  patch "/cancel_subscription", to: "subscriptions#cancel"
+  get "/subscriptions", to: "subscriptions#index"
+
 end
