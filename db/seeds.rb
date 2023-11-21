d161ed7b2d9ecd5beb962705996b3273fe65987b
Antoine Aube <aaube3@gmail.com>
2023-11-20 18:30:44 -0800

    db: add seed specifically for postman test

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,11 @@ end
 3.times do 
   FactoryBot.create(:tea)
 end
+
+customer4 = Customer.create(first_name: "John", last_name: "Doe", email: "john@gmail.com", address: "123 John Street")
+tea1 = Tea.create(title: "Green Tea", description: "Green tea", temperature: 75, brew_time: 5)
+tea2 = Tea.create(title: "Blue Tea", description: "blue tea", temperature: 90, brew_time: 5)
+subscription1 = Subscription.create(customer_id: customer4.id, title: "Single Tea")
+subscription2 = Subscription.create(customer_id: customer4.id, title: "Single Tea")
+tea_subscription1 = TeaSubscription.create(tea_id: tea1.id, subscription_id: subscription1.id)
+tea_subscription2 = TeaSubscription.create(tea_id: tea2.id, subscription_id: subscription2.id)
